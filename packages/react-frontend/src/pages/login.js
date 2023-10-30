42a01d09f7bcc872c5e444b12e12ced9599a551e
Luke Waltz <lukewaltz@outlook.com>
2023-10-30 09:42:07 -0700

    feat: some changes to login

--- packages/react-frontend/src/pages/login.js
+++ packages/react-frontend/src/pages/login.js
@@ -1,8 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import UserForm from "../components/UserForm";
  
 const Login = () => {
+    // const [characters, setCharacters] = useState([])
+
+    // function postUser(user){
+    //     const promise = fetch("http://localhost:8000/users", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(user),
+    //     })
+    //     //console.log("in postUser")
+    //         .then((res) => (res.status === 201 ? res.json() : undefined))
+    //         .then((json) => {
+    //             if (json) {
+    //                 console.log(json);
+    //                 setCharacters([...characters, json]);
+    //                 console.log(characters);
+    //             }
+    //         });
+    //     return promise;
+    // }
+
     return (
         <div className="container">
             <h1>
