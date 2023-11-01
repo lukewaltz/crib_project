ff84d1b3badb5f126198069b13b87b00d2cd30a5
Luke Waltz <lukewaltz@outlook.com>
2023-10-31 20:29:25 -0700

    feat: added postTask request (untested)

--- packages/react-frontend/src/pages/home.js
+++ packages/react-frontend/src/pages/home.js
@@ -13,6 +13,41 @@ const Home = () => {
 	  setCharacters(updated);
 	}
 
+  function postTask(chore){
+    const taskData = { task: chore};
+
+    fetch("http://localhost:8000/tasks", 
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application.json",
+      },
+      body: JSON.stringify(taskData),
+    })
+    .then((response) => {
+      if (response.status === 201) {
+        // Task added successfully
+        console.log('Task added successfully');
+        return response.json();
+      } else if (response.status === 500) {
+        // Could not add task, handle as needed
+        console.log('Could not add task');
+        throw new Error('Could not add task');
+      } else {
+        // Handle other responses as needed
+        throw new Error('Task could not be added');
+      }
+    })
+    .then((task) => {
+      // Update your component state with the added task
+      setCharacters([...characters, task]);
+    })
+    .catch((error) => {
+      console.error(error);
+      // Handle error
+    });
+  }
+
   function updateList(chore) {
     setCharacters([...characters, chore]);
   }
