2891526d970b22435380d5952857a59d71acf5f1
Luke Waltz <lukewaltz@outlook.com>
2023-11-08 09:24:22 -0800

    feat: added POST request for poll in post.js

--- packages/react-frontend/src/pages/post.js
+++ packages/react-frontend/src/pages/post.js
@@ -31,7 +31,26 @@ export const Post = (props) => {
 
     function handlePollSubmit(poll){
         console.log(poll.poll)
-
+        
+        fetch('http://localhost:8000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(poll),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Poll posted:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     }
 
     return (
