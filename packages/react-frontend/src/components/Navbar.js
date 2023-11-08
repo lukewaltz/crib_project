dc7144d4d704fc7574a50a0fe450aa8d527fb472
brraymun <your_email@youremail.com>
2023-11-08 09:18:25 -0800

    merging conflicts

--- packages/react-frontend/src/components/Navbar.js
+++ packages/react-frontend/src/components/Navbar.js
@@ -26,6 +26,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
--- packages/react-frontend/src/components/UserForm.js
+++ packages/react-frontend/src/components/UserForm.js
@@ -1,16 +1,5 @@
 import React, {useState} from 'react';
 function UserForm(props) {
-<<<<<<< HEAD
-  const [user, setUser] = useState({
-    username: "",
-    name: "",
-    email: "",
-  });
-
-  function submitForm() {
-    props.handleSubmit(user);
-    setUser({ username: '', name: '', email: '' });
-=======
   const [user, setUser] = useState(
      {
         username: "",
@@ -22,14 +11,10 @@ function UserForm(props) {
   function submitForm() {
     props.handleSubmit(user);
     setUser({username: '', password: '', email: ''});
->>>>>>> origin
   }
 
   function handleChange(event) {
     const { name, value } = event.target;
-<<<<<<< HEAD
-    setUser({ ...user, [name]: value });
-=======
     if (name === "password")
       setUser(
          {username: user['username'], password: value, email: user['email']}
@@ -42,40 +27,9 @@ function UserForm(props) {
         setUser(
             {username: user['username'], password: user['password'], email: value}
         );
->>>>>>> origin
   }
 
   return (
-<<<<<<< HEAD
-    <form>
-      <label htmlFor="username">Username</label>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        value={user.username}
-        onChange={handleChange}
-      />
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        value={user.name}
-        onChange={handleChange}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        value={user.email}
-        onChange={handleChange}
-      />
-      <input type="button" value="Login" onClick={submitForm} />
-    </form>
-  );
-=======
         <form>
         <label htmlFor="username">Username</label>
         <input
@@ -102,7 +56,6 @@ function UserForm(props) {
         </form>
     );
 
->>>>>>> origin
 }
 
 export default UserForm;
